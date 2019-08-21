7e416d606bf3d5781b0f4e06d9bf7837611c45fb
inventivetalent <mail@inventivetalent.org>
2019-08-21 23:01:46 +0200

    stuff

--- routes/activeUsers.js
+++ routes/activeUsers.js
@@ -1,6 +1,6 @@
 module.exports = function (vars, pool) {
     return function (req, res) {
-        pool.query("SELECT COUNT(*) AS total FROM hypixel_skyblock_magma_timer_pings WHERE active_time > NOW() - INTERVAL 2 MINUTE", function (err, results) {
+        pool.query("SELECT COUNT(id) AS total FROM hypixel_skyblock_magma_timer_pings WHERE active_time > NOW() - INTERVAL 2 MINUTE", function (err, results) {
             if (err) {
                 console.warn(err);
                 res.json({
