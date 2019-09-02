1a96c98ed2bc4dff4007e0967b16391bb15c16ca
inventivetalent <mail@inventivetalent.org>
2019-09-02 20:30:26 +0200

    use connection instead

--- routes/magmaBossTimer/activeUsers.js
+++ routes/magmaBossTimer/activeUsers.js
@@ -1,32 +1,48 @@
 module.exports = function (vars, pool) {
     return function (req, res) {
-        pool.query("SELECT COUNT(id) AS total FROM skyblock_magma_timer_pings WHERE time > NOW() - INTERVAL 2 MINUTE", function (err, results) {
+        pool.getConnection(function(err,connection){
             if (err) {
                 console.warn(err);
-                res.json({
+                res.status(500).json({
                     success: false,
-                    msg: "sql error"
+                    msg: "Failed to get connection from pool"
                 });
                 return;
             }
-            pool.query("SELECT COUNT(id) AS total FROM `skyblock_magma_timer_ips` WHERE time > NOW() - INTERVAL 2 HOUR", function (err, results1) {
+
+            connection.query("SELECT COUNT(id) AS total FROM skyblock_magma_timer_pings WHERE time > NOW() - INTERVAL 2 MINUTE", function (err, results) {
                 if (err) {
                     console.warn(err);
                     res.json({
                         success: false,
                         msg: "sql error"
                     });
+                    connection.release();
                     return;
                 }
+                connection.query("SELECT COUNT(id) AS total FROM `skyblock_magma_timer_ips` WHERE time > NOW() - INTERVAL 2 HOUR", function (err, results1) {
+                    if (err) {
+                        console.warn(err);
+                        res.json({
+                            success: false,
+                            msg: "sql error"
+                        });
+                        connection.release();
+                        return;
+                    }
 
-                res.set("Cache-Control","public, max-age=120");
-                res.json({
-                    success: true,
-                    msg: "",
-                    activeUsers: results[0].total,
-                    usersLastTwoHrs: results1[0].total
+                    res.set("Cache-Control","public, max-age=120");
+                    res.json({
+                        success: true,
+                        msg: "",
+                        activeUsers: results[0].total,
+                        usersLastTwoHrs: results1[0].total
+                    })
+                    connection.release();
                 })
             })
-        })
+        });
+
+
     }
 };
