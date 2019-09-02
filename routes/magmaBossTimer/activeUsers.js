d1e1f4666afba58794e67bfed2db31d5b288d616
inventivetalent <mail@inventivetalent.org>
2019-09-02 20:29:07 +0200

    add another thing

--- routes/magmaBossTimer/activeUsers.js
+++ routes/magmaBossTimer/activeUsers.js
@@ -9,12 +9,23 @@ module.exports = function (vars, pool) {
                 });
                 return;
             }
+            pool.query("SELECT COUNT(id) AS total FROM `skyblock_magma_timer_ips` WHERE time > NOW() - INTERVAL 2 HOUR", function (err, results1) {
+                if (err) {
+                    console.warn(err);
+                    res.json({
+                        success: false,
+                        msg: "sql error"
+                    });
+                    return;
+                }
 
-            res.set("Cache-Control","public, max-age=60");
-            res.json({
-                success: true,
-                msg: "",
-                activeUsers: results[0].total
+                res.set("Cache-Control","public, max-age=120");
+                res.json({
+                    success: true,
+                    msg: "",
+                    activeUsers: results[0].total,
+                    usersLastTwoHrs: results1[0].total
+                })
             })
         })
     }
